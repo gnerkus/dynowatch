218fe2e9e85fc3839ac6d81961d7b39ece68dac0
gnerkus <ifeanyioraelosi@gmail.com>
2016-07-27 14:06:48 +0100

    [Parser]: define specs for obtaining data from a log line

--- spec/dynowatch_spec.rb
+++ spec/dynowatch_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 
 describe Dynowatch::Parser do
 
+  let(:valid_log) { '2014-01-09T06:16:53.742892+00:00 heroku[router]: at=info method=GET path=/api/users/100002266342173/count_pending_messages host=services.pocketplaylab.com fwd="94.66.255.106" dyno=web.8 connect=9ms service=9ms status=304 bytes=0' }
+
+  let(:invalid_log) { 'x.x.x.90 - - [13/Sep/2006:07:01:51 -0700] "PROPFIND /svn/[xxxx]/[xxxx]/trunk HTTP/1.1" 401 587' }
+
   describe 'is_valid_log' do
-    let(:valid_log) { '2014-01-09T06:16:53.742892+00:00 heroku[router]: at=info method=GET path=/api/users/100002266342173/count_pending_messages host=services.pocketplaylab.com fwd="94.66.255.106" dyno=web.8 connect=9ms service=9ms status=304 bytes=0' }
-
-    let(:invalid_log) { 'x.x.x.90 - - [13/Sep/2006:07:01:51 -0700] "PROPFIND /svn/[xxxx]/[xxxx]/trunk HTTP/1.1" 401 587' }
-
     it 'should return true for valid log string' do
       expect(Dynowatch::Parser.is_valid_log(valid_log)).to be true
     end
@@ -17,4 +17,27 @@ describe Dynowatch::Parser do
     end
   end
 
+  describe 'get_route_name' do
+    it 'should determine the matching route from a log line' do
+      expect(Dynowatch::Parser.get_route_name(valid_log)).to eq('pending_messages')
+    end
+  end
+
+  describe 'get_time' do
+    it 'should fetch the response time' do
+      expect(Dynowatch::Parser.get_time(valid_log)).to eq(18)
+    end
+  end
+
+  describe 'get_dyno' do
+    it 'should fetch the responding dyno' do
+      expect(Dynowatch::Parser.get_dyno(valid_log)).to eq('web.8')
+    end
+  end
+
 end
+
+# Info required:
+# - url (including request type)
+# - response time
+# - dyno
