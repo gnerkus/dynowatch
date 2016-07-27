87f2a138aceaf6411076db77aa9bdb956c2b7e36
gnerkus <ifeanyioraelosi@gmail.com>
2016-07-27 13:44:53 +0100

    [Parser] check for valid log lines not entire file

--- lib/dynowatch/parser.rb
+++ lib/dynowatch/parser.rb
@@ -1,24 +1,14 @@
 module Dynowatch
   class Parser
-    # A valid Heroku log file has the form:
+    # A valid Heroku log has the form:
     # time hostname heroku[router]: <log-details>
     # Example:
     # 2014-01-09T06:16:53.742892+00:00 heroku[router]: at=info method=GET path=/api/users/100002266342173/count_pending_messages host=services.pocketplaylab.com fwd="94.66.255.106" dyno=web.8 connect=9ms service=9ms status=304 bytes=0
     # We're only concerned with the format at the beginning.
-    def self.is_valid_log(log_file)
-      is_valid = true
+    def self.is_valid_log(log)
       line_regex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}\+00:00\ heroku\[router\]\:/
 
-      # Open log file for reading
-      File.open(log_file, 'r') do |infile|
-        while line = infile.gets
-          if !(line =~ line_regex)
-            is_valid = false
-          end
-        end
-      end
-
-      is_valid
+      !!(log =~ line_regex)
     end
   end
 end
--- spec/dynowatch_spec.rb
+++ spec/dynowatch_spec.rb
@@ -4,15 +4,16 @@ require 'spec_helper'
 describe Dynowatch::Parser do
 
   describe 'is_valid_log' do
-    let(:valid_log_file) { 'valid.log' }
-    let(:invalid_log_file) { 'invalid.log' }
+    let(:valid_log) { '2014-01-09T06:16:53.742892+00:00 heroku[router]: at=info method=GET path=/api/users/100002266342173/count_pending_messages host=services.pocketplaylab.com fwd="94.66.255.106" dyno=web.8 connect=9ms service=9ms status=304 bytes=0' }
 
-    it 'should return true for valid log file' do
-      expect(Dynowatch::Parser.is_valid_log(valid_log_file)).to be true
+    let(:invalid_log) { 'x.x.x.90 - - [13/Sep/2006:07:01:51 -0700] "PROPFIND /svn/[xxxx]/[xxxx]/trunk HTTP/1.1" 401 587' }
+
+    it 'should return true for valid log string' do
+      expect(Dynowatch::Parser.is_valid_log(valid_log)).to be true
     end
 
-    it 'should return false for invalid log file' do
-      expect(Dynowatch::Parser.is_valid_log(invalid_log_file)).to be false
+    it 'should return false for invalid log string' do
+      expect(Dynowatch::Parser.is_valid_log(invalid_log)).to be false
     end
   end
 
