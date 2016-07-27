0d1b3d2c87b064680db45e1335cb24e7209a5a78
gnerkus <ifeanyioraelosi@gmail.com>
2016-07-27 11:53:43 +0100

    [Parser] define tests for log file validation and create test log files

--- spec/dynowatch_spec.rb
+++ spec/dynowatch_spec.rb
@@ -1,11 +1,19 @@
+require 'dynowatch'
 require 'spec_helper'
 
-describe Dynowatch do
-  it 'has a version number' do
-    expect(Dynowatch::VERSION).not_to be nil
+describe Dynowatch::Parser do
+
+  describe 'is_valid_log' do
+    let(:valid_log_file) { 'valid.log' }
+    let(:invalid_log_file) { 'invalid.log' }
+
+    it 'should return true for valid log file' do
+      expect(Dynowatch::Parser.is_valid_log(valid_log_file)).to be true
+    end
+
+    it 'should return false for invalid log file' do
+      expect(Dynowatch::Parser.is_valid_log(invalid_log_file)).to be false
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
